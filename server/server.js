66fa1af3b9ed9f329e04c58c0d5311e7b076b53e
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-10-12 12:08:22 +0530

    added refresh token to the prisma scheme

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const passport = require("passport");
+
+const loginRoute = require("./routes/loginRoute");
 
 const app = express();
+app.use(express.json());
 
-const PORT = process.env.PORT || 3000
+app.use("/login", loginRoute);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port no ${PORT}`)
-})
+  console.log(`Server running on port no ${PORT}`);
+});
