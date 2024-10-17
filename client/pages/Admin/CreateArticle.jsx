e86faf461174b9ef4bb2b15c498f58f866ec88fb
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-10-17 16:45:31 +0530

    design: buttons for create post

--- client/pages/Admin/CreateArticle.jsx
+++ client/pages/Admin/CreateArticle.jsx
@@ -7,8 +7,8 @@ function CreateArticle() {
 
   const API_KEY = import.meta.env.VITE_MCE_API_KEY;
 
-  const handleEditorChange = (content, editor) => {
-    setContent(content); // Update content state when TinyMCE editor changes
+  const handleEditorChange = (content) => {
+    setContent(content);
   };
 
   return (
@@ -62,6 +62,24 @@ function CreateArticle() {
                 "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
             }}
           />
+        </div>
+        <div className="flex flex-row items-center gap-6 mt-6">
+          <div className="submitButton">
+            <button
+              className="bg-yellow-300 hover:bg-yellow-400 w-full text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-2xl"
+              type="submit"
+            >
+              Publish
+            </button>
+          </div>
+          <div className="draftButton">
+            <button
+              className="bg-slate-400 hover:bg-slate-500 w-full text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-2xl"
+              type="submit"
+            >
+              Save in Drafts
+            </button>
+          </div>
         </div>
       </form>
     </div>
